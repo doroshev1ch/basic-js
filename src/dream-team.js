7bc4b17f68d65ad289f1d5ad66b7ddf8e8d0c796
doroshev1ch <doroshevich.2002@mail.ru>
2023-04-13 00:02:06 +0300

    feat: complite one of the tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -14,17 +14,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function createDreamTeam(members) {
-  let team = [];
-  for (let i = 0; i < members.length; i++){
-    if (typeof members[i] === 'string'){
-      team.push(members[i].replace(/ /g,'')[0]);
-    } else if (typeof members[i] != 'string') {
-      team.push('');
-    } else if (!Array.isArray(members)) {
-      return false;
+  let team = '';
+  if (typeof members !== 'undefined' && members !== null && members.length > 0) {
+    for (let i = 0; i < members.length; i++){
+      if (typeof members[i] === 'string'){
+        team += members[i].replace(/ /g,'')[0];
+      } else if (typeof members[i] != 'string') {
+        team += '';
+      } else {
+        return false;
+      }
     }
   }
-  return team.sort().join('').toUpperCase()
+  return team.toUpperCase().split('').sort().join('');
 }
 
 module.exports = {
